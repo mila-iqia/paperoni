5b806b1af884667ba659a61049dcbf8c84245d33
Olivier Breuleux <breuleux@gmail.com>
2020-12-01 21:51:27 -0500

    Add some tests

---
+++ tests/common.py
@@ -0,0 +1,61 @@
+import os
+
+import pytest
+
+
+@pytest.fixture
+def researchers():
+    from paperoni.io import ResearchersFile
+
+    here = os.path.dirname(__file__)
+    return ResearchersFile(os.path.join(here, "rsch.json"))
+
+
+@pytest.fixture
+def researcher(researchers):
+    return researchers.get("olivier breuleux")
+
+
+@pytest.fixture
+def precollected(researchers):
+    from paperoni.io import PapersFile
+
+    here = os.path.dirname(__file__)
+    return PapersFile(os.path.join(here, "oli.json"), researchers=researchers)
+
+
+@pytest.fixture
+def some_paper(precollected):
+    results = precollected.query(
+        {
+            "title": "Automatic differentiation in ML: Where we are and where we should be going",
+            "venue": "NeurIPS",
+        }
+    )
+    assert len(results) == 1
+    return results[0]
+
+
+@pytest.fixture
+def some_other_paper(precollected):
+    results = precollected.query(
+        {
+            "title": " Quickly generating representative samples from an rbm-derived process",
+        }
+    )
+    assert len(results) == 1
+    return results[0]
+
+
+@pytest.fixture
+def apikey():
+    key = os.getenv("PAPERONI_API_KEY")
+    if key is None:
+        raise Exception("$PAPERONI_API_KEY must be set to execute test")
+    return key
+
+
+@pytest.fixture
+def qm(apikey):
+    from paperoni.query import QueryManager
+    return QueryManager(apikey)
---
+++ tests/test_utils.py
@@ -0,0 +1,21 @@
+from paperoni.utils import asciiify, group_by, normalize, print_field
+
+
+def test_asciiify():
+    assert asciiify("Le café est brûlant") == "Le cafe est brulant"
+    assert asciiify("cool ↔ beans") == "cool  beans"
+
+
+def test_normalize():
+    assert normalize(None) is None
+    assert normalize("Le café est brûlant") == "le cafe est brulant"
+    assert normalize("cool ↔ beans") == "cool  beans"
+
+
+def test_group_by():
+    numbers = [-1, 9, 73, 0, -91, 0, -8]
+    grp = group_by(numbers, key=lambda x: x < 0)
+    assert grp == {
+        True: [-1, -91, -8],
+        False: [9, 73, 0, 0],
+    }
