c8471bc35ce3843928e7f84151cee04ec56e482c
notoraptor <stevenbocco@gmail.com>
2021-10-18 08:36:01 -0400

    Add command `semantic-scholar -k <keywords>` to search by keywords using Semantic Scholar API.

--- paperoni/semantic_scholar.py
+++ paperoni/semantic_scholar.py
@@ -6,10 +6,6 @@ from paperoni.query import QueryError
 import pprint
 
 
-def _print(data):
-    pprint.PrettyPrinter(indent=2).pprint(data)
-
-
 def _paper_long_fields(parent=None, extras=()):
     fields = (
         "paperId",
@@ -94,7 +90,7 @@ class SemanticScholarQueryManager:
     def __init__(self):
         self.conn = http.client.HTTPSConnection("api.semanticscholar.org")
 
-    def evaluate(self, path: str, fields: tuple = None, **params):
+    def _evaluate(self, path: str, fields: tuple = None, **params):
         params = {k: v for k, v in params.items() if v is not None}
         if fields is not None:
             params["fields"] = ",".join(fields)
@@ -111,7 +107,7 @@ class SemanticScholarQueryManager:
         self, query, offset=0, limit=100, fields=SEARCH_FIELDS,
     ):
         # {total: str, offset: int, next: optinal int, data: [...]}
-        return self.evaluate(
+        return self._evaluate(
             "paper/search",
             query=query,
             offset=offset,
@@ -121,13 +117,13 @@ class SemanticScholarQueryManager:
 
     def paper(self, paper_id, fields=PAPER_FIELDS):
         # paper dict
-        return self.evaluate(f"paper/{paper_id}", fields=fields)
+        return self._evaluate(f"paper/{paper_id}", fields=fields)
 
     def paper_authors(
         self, paper_id, offset=0, limit=100, fields=PAPER_AUTHORS_FIELDS,
     ):
         # {offset: int, next: optional int, data: [...]}
-        return self.evaluate(
+        return self._evaluate(
             f"paper/{paper_id}/authors",
             offset=offset,
             limit=limit,
@@ -138,7 +134,7 @@ class SemanticScholarQueryManager:
         self, paper_id, offset=0, limit=100, fields=PAPER_CITATIONS_FIELDS,
     ):
         # {offset: int, next: optional int, data: [...]}
-        return self.evaluate(
+        return self._evaluate(
             f"paper/{paper_id}/citations",
             offset=offset,
             limit=limit,
@@ -149,7 +145,7 @@ class SemanticScholarQueryManager:
         self, paper_id, offset=0, limit=100, fields=PAPER_REFERENCES_FIELDS,
     ):
         # {offset: int, next: optional int, data: [...]}
-        return self.evaluate(
+        return self._evaluate(
             f"paper/{paper_id}/citations",
             offset=offset,
             limit=limit,
@@ -158,27 +154,15 @@ class SemanticScholarQueryManager:
 
     def author(self, author_id, fields=AUTHOR_FIELDS):
         # author dict
-        return self.evaluate(f"author/{author_id}", fields=fields)
+        return self._evaluate(f"author/{author_id}", fields=fields)
 
     def author_papers(
         self, author_id, offset=0, limit=100, fields=AUTHOR_PAPERS_FIELDS,
     ):
         # {offset: int, next: optional int, data: [...]}
-        return self.evaluate(
+        return self._evaluate(
             f"author/{author_id}/papers",
             offset=offset,
             limit=limit,
             fields=fields,
         )
-
-    def test(self):
-        # _print(self.search("paleontology", fields=()))
-        # _print(self.paper("84deebbb20d312acc58785cf58a9e5dd445b4cf4"))
-        # _print(self.paper_authors("84deebbb20d312acc58785cf58a9e5dd445b4cf4"))
-        # _print(self.paper_citations("84deebbb20d312acc58785cf58a9e5dd445b4cf4"))
-        # _print(self.paper_references("84deebbb20d312acc58785cf58a9e5dd445b4cf4"))
-        # _print(self.author("11557131"))
-        _print(self.author_papers("11557131"))
-
-
-SemanticScholarQueryManager().test()
