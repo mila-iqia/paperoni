f1df9c8121b0d71804432e9f3eb122abaa59a0d0
notoraptor <stevenbocco@gmail.com>
2021-11-25 08:32:39 -0500

    Add boolean column `paper.excluded` to manage excluded papers. Check paper IDs to find a paper in SQL database.

--- paperoni/commands/command_collect_semantic_scholar.py
+++ paperoni/commands/command_collect_semantic_scholar.py
@@ -1,3 +1,4 @@
+import pprint
 from coleo import Option, default, tooled
 
 from ..papers2 import Paper
@@ -22,7 +23,10 @@ class Collection:
             return True
         else:
             return self.db.select_id_from_values(
-                "paper", "paper_id", title=paper.title, abstract=paper.abstract
+                "paper",
+                "paper_id",
+                paper_id=self._find_paper_id(paper),
+                excluded=0
             )
 
     def add(self, paper: Paper):
@@ -36,46 +40,69 @@ class Collection:
         self.papers_excluded.add(paper)
 
     def excludes(self, paper: Paper):
-        return paper in self.papers_excluded
+        return paper in self.papers_excluded or self.db.select_id_from_values(
+            "paper",
+            "paper_id",
+            paper_id=self._find_paper_id(paper),
+            excluded=1,
+        )
 
     def save(self):
-        # Delete excluded papers.
-        for paper in self.papers_excluded:
-            if paper.abstract is None:
-                self.db.modify(
-                    "DELETE FROM paper WHERE title = ? AND abstract IS NULL",
-                    [paper.title],
-                )
-            else:
-                self.db.modify(
-                    "DELETE FROM paper WHERE title = ? AND abstract = ?",
-                    (paper.title, paper.abstract),
-                )
-        # Add or update papers.
-        for paper in self.papers_added:
-            self._save_paper(paper)
+        try:
+            # Exclude papers.
+            for paper in self.papers_excluded:
+                self._create_paper_entry(paper, excluded=1)
+            # Add or update papers.
+            for paper in self.papers_added:
+                self._save_paper(paper)
+        except Exception as exc:
+            raise RuntimeError(f"Error saving paper: {pprint.pformat(paper)}") from exc
+
+    def _find_paper_id(self, paper: Paper):
+        # Check paper IDs then title+abstract.
+        for link_type in (
+            "SemanticScholar",
+            "MAG",
+            "ACL",
+            "DBLP",
+            "DOI",
+            "PubMed",
+            "PubMedCentral",
+        ):
+            paper_id = self.db.select_id_from_values(
+                "paper_link",
+                "paper_id",
+                link_type=link_type,
+                link=paper.get_ref(link_type),
+            )
+            if paper_id is not None:
+                return paper_id
+        return self.db.select_id_from_values(
+            "paper", "paper_id", title=paper.title, abstract=paper.abstract
+        )
+
+    def _create_paper_entry(self, paper: Paper, excluded: int = 0) -> int:
+        paper_id = (
+            self._find_paper_id(paper)
+            or self.db.insert(
+                "paper",
+                ("title", "abstract", "citation_count"),
+                (paper.title, paper.abstract, paper.citation_count),
+            )
+        )
+        # Set excluded.
+        self.db.modify(
+            "UPDATE OR IGNORE paper SET excluded = ? WHERE paper_id = ?",
+            (excluded, paper_id),
+        )
+        return paper_id
 
     def _save_paper(self, paper: Paper):
         db = self.db
         author_indices = []
         topic_indices = []
         # Paper: check MAG ID then title+abstract.
-        paper_id = (
-            db.select_id_from_values(
-                "paper_link",
-                "paper_id",
-                link_type="MAG",
-                link=paper.get_ref("MAG"),
-            )
-            or db.select_id_from_values(
-                "paper", "paper_id", title=paper.title, abstract=paper.abstract
-            )
-            or db.insert(
-                "paper",
-                ("title", "abstract", "citation_count"),
-                (paper.title, paper.abstract, paper.citation_count),
-            )
-        )
+        paper_id = self._create_paper_entry(paper, excluded=0)
         # Links
         for link in paper.links:
             if not db.count(
@@ -122,7 +149,10 @@ class Collection:
             # Release
             (release,) = paper.releases
             release_date = None
-            release_year = int(release.year)
+            # NB: Even release.year may be None, so
+            # we set it to an invalid year like -1 000 000,
+            # as we don't expect humans to have written something in such year.
+            release_year = int(release.year) if release.year else -1000000
             volume = venue_volume
             release_id = db.select_id_from_values(
                 "release",
@@ -158,7 +188,7 @@ class Collection:
         for author_position, (author_id, author) in enumerate(author_indices):
             # Author affiliations may be empty, but we must still
             # save paper to author relation.
-            for affiliation in (author.affiliations or [""]):
+            for affiliation in author.affiliations or [""]:
                 db.modify(
                     "INSERT OR IGNORE INTO paper_author "
                     "(paper_id, author_id, author_position, affiliation) "
