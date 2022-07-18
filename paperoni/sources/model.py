99e66348bea11554f8af62e949acbedcaaa5111b
Olivier Breuleux <breuleux@gmail.com>
2022-07-18 14:26:15 -0400

    Adjustments to the model

--- paperoni/sources/model.py
+++ paperoni/sources/model.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
-from pydantic import BaseModel
+
+import re
 from datetime import datetime
 from enum import Enum
 from typing import Optional
+
+from pydantic import BaseModel
 
 
 class VenueType(str, Enum):
@@ -10,10 +13,84 @@ class VenueType(str, Enum):
     conference = "conference"
     workshop = "workshop"
     symposium = "symposium"
+    book = "book"
+    review = "review"
     unknown = "unknown"
 
 
-class Paper(BaseModel):
+class InstitutionCategory(str, Enum):
+    industry = "industry"
+    academia = "academia"
+    unknown = "unknown"
+
+
+class DatePrecision(int, Enum):
+    unknown = 0
+    year = 1
+    month = 2
+    day = 3
+
+    @staticmethod
+    def assimilate_date(date):
+        match date:
+            case int() as year:
+                return {
+                    "date": f"{year}-01-01 00:00",
+                    "date_precision": DatePrecision.year,
+                }
+            case str() if m := re.match("^(....)-(..)-(..).*", date):
+                match m.groups():
+                    case (year, month, day):
+                        if day == "01":
+                            if month == "01":
+                                precision = DatePrecision.year
+                            else:
+                                # precision = DatePrecision.month
+                                precision = DatePrecision.day
+                        else:
+                            precision = DatePrecision.day
+                        return {
+                            "date": f"{year}-{month}-{day} 00:00",
+                            "date_precision": precision,
+                        }
+                    case _:
+                        assert False
+            case _:
+                assert False
+
+    @staticmethod
+    def format(date, precision):
+        match DatePrecision(precision):
+            case DatePrecision.year | DatePrecision.unknown:
+                return date.strftime("%Y")
+            case DatePrecision.month:
+                return date.strftime("%Y-%m")
+            case DatePrecision.day:
+                return date.strftime("%Y-%m-%d")
+            case _:
+                assert False
+
+    def format2(self, date):
+        match self:
+            case DatePrecision.year | DatePrecision.unknown:
+                return date.strftime("%Y")
+            case DatePrecision.month:
+                return date.strftime("%Y-%m")
+            case DatePrecision.day:
+                return date.strftime("%Y-%m-%d")
+            case _:
+                assert False
+
+
+class Base(BaseModel):
+    def tagged_json(self):
+        return self.__config__.json_dumps(
+            {"__type__": type(self).__name__, **self.dict()},
+            default=self.__json_encoder__,
+        )
+
+
+class Paper(Base):
     title: str
     abstract: str
     citation_count: int
@@ -21,39 +98,51 @@ class Paper(BaseModel):
     releases: list[Release]
     topics: list[Topic]
     links: list[Link]
+    scrapers: list[str]
 
 
-class Author(BaseModel):
+class Author(Base):
     name: str
-    affiliations: list[Affiliation]
+    affiliations: list[Institution]
+    roles: list[Role]
+    aliases: list[str]
     links: list[Link]
 
 
-class Affiliation(BaseModel):
+class Institution(Base):
     name: str
+    category: InstitutionCategory
+    aliases: list[str]
 
 
-class Release(BaseModel):
+class Release(Base):
     venue: Venue
     date: datetime
-    date_precision: int
+    date_precision: DatePrecision
     volume: Optional[str]
     publisher: Optional[str]
 
 
-class Venue(BaseModel):
+class Venue(Base):
     type: VenueType
     name: str
     links: list[Link]
 
 
-class Topic(BaseModel):
+class Topic(Base):
     name: str
 
 
-class Link(BaseModel):
+class Link(Base):
     type: str
     link: str
+
+
+class Role(Base):
+    institution: Institution
+    role: str
+    start_date: datetime
+    end_date: datetime | None
 
 
 for cls in list(globals().values()):
