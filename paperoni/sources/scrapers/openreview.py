5b94e18b292290f8aa48d22ee79015704d086d56
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2022-07-29 15:30:28 -0400

    Add OpenReview scraper acquire method

--- paperoni/sources/scrapers/openreview.py
+++ paperoni/sources/scrapers/openreview.py
@@ -26,16 +26,7 @@ class OpenReviewScraper:
             return VenueType.unknown
 
     @staticmethod
-    def _query(client, params):
-        venue = params["content"].get(["venueid"], None)
-
-        for v_type in VenueType:
-            if v_type in venue.lower():
-                venue_type = v_type
-                break
-        else:
-            venue_type = VenueType.unknown
-
+    def _query(client, params, total=0, limit=1000000):
         next_offset = 0
         while total < limit:
             params["offset"] = next_offset
@@ -158,103 +149,29 @@ class OpenReviewScraper:
                 "content": {**params["content"], "venueid": v},
             }
 
-            for v_type in VenueType:
-                if v_type in v.lower():
-                    venue_type = v_type
-                    break
-            else:
-                venue_type = VenueType.unknown
-
-            next_offset = 0
-            while total < limit:
-                params["offset"] = next_offset
-                notes = client.get_all_notes(**params)
-                for note in notes:
-                    authors = []
-                    if len(note.content["authors"]) == len(
-                        note.content.get("authorids", [])
-                    ) and all(
-                        (
-                            aid.startswith("~")
-                            for aid in note.content["authorids"]
-                        )
-                    ):
-                        authors_ids = note.content["authorids"]
-                    else:
-                        authors_ids = (
-                            None for _ in range(len(note.content["authors"]))
-                        )
-                    for name, author_id in zip(
-                        note.content["authors"], authors_ids
-                    ):
-                        _links = []
-                        if author_id:
-                            _links.append(
-                                Link(type="openreview", link=author_id)
-                            )
-                        authors.append(
-                            PaperAuthor(
-                                affiliations=[],
-                                author=Author(
-                                    name=name,
-                                    affiliations=[],
-                                    aliases=[],
-                                    links=_links,
-                                    roles=[],
-                                ),
-                            )
-                        )
-                    _links = [Link(type="openreview", link=note.id)]
-                    if "code" in note.content:
-                        Link(type="git", link=note.content["code"])
-                    yield Paper(
-                        title=note.content["title"],
-                        abstract=note.content.get("abstract", ""),
-                        citation_count=0,
-                        authors=authors,
-                        releases=[
-                            Release(
-                                venue=Venue(
-                                    type=venue_type,
-                                    name=note.content["venue"],
-                                    links=[],
-                                ),
-                                date=str(
-                                    datetime.fromtimestamp(note.tcdate / 1000)
-                                ),
-                                date_precision=DatePrecision.day,
-                            )
-                        ],
-                        topics=[
-                            Topic(name=kw)
-                            for kw in note.content.get("keywords", [])
-                        ],
-                        links=_links,
-                        scrapers=["orev"],
-                    )
-                next_offset += len(notes)
-                if not notes:
-                    break
-            total += next_offset
+            for paper in OpenReviewScraper._query(client, params, total, limit):
+                total += 1
+                yield paper
 
     @tooled
     def acquire(self, queries):
-        emails_todo = {}
-        ids_todo = {}
+        todo = {}
 
         for auq in queries:
             for link in auq.author.links:
-                if link.type == "email":
-                    emails_todo[link.link] = auq
-                elif link.type == "openreview":
-                    ids_todo[link.link] = auq
+                if link.type == "openreview":
+                    todo[link.link] = auq
 
-        for email, auq in emails_todo.items():
-            print(f"Fetch papers for {auq.author.name} (email={email})")
+        client = openreview.Client(baseurl="https://api.openreview.net")
+
+        for author_id, auq in todo.items():
+            print(f"Fetch papers for {auq.author.name} (email={author_id})")
             params = {
-                "content": {"author_emails": email},
+                "content": {"authorids": [author_id]},
+                "mintcdate": int(auq.start_date.timestamp() * 1000),
             }
-            yield self.query()
+            for paper in OpenReviewScraper._query(client, params):
+                yield paper
 
     @tooled
     def prepare(self, researchers):
