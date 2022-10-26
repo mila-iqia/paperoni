c21f463fae551cf35de35a375b7a9e9a237d391f
Olivier Breuleux <breuleux@gmail.com>
2022-10-26 16:09:15 -0400

    Improvements in tests and test dbs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,57 @@
 from pathlib import Path
+import shutil
 
 from pytest import fixture
 
 
-@fixture
-def config():
+def _ensure_db(cfg):
+    # Path of the writable database
+    dbpath = cfg.paths.database
+    # Path of the read-only database that serves as a model
+    dbfixed = dbpath.with_suffix(".db.fixed")
+    # Path to a JSONL history file that can be replayed to create the db
+    dbhistory = dbpath.with_suffix(".jsonl")
+
+    dbfixed_t = dbfixed.stat().st_mtime if dbfixed.exists() else 0
+    dbhistory_t = dbhistory.stat().st_mtime if dbhistory.exists() else 0
+
+    # Either of these are required to exist
+    assert dbfixed_t or dbhistory_t
+
+    if dbhistory_t > dbfixed_t:
+        # Remove any existing db
+        dbpath.unlink(missing_ok=True)
+        # This replays the history into dbpath
+        cfg.database.replay(history=dbhistory)
+        # Cache the result of the replay
+        shutil.copy(dbpath, dbfixed)
+
+    elif dbpath.exists() and not cfg.writable:
+        return
+
+    else:
+        shutil.copy(dbfixed, dbpath)
+
+
+def transient_config(name):
     from paperoni.config import load_config
 
-    default_config = Path(__file__).parent / "data" / "config.yaml"
-    with load_config(default_config) as cfg:
+    config_file = Path(__file__).parent / "data" / name
+    with load_config(config_file) as cfg:
+        _ensure_db(cfg)
         yield cfg
 
 
 @fixture
-def transient_config():
-    from paperoni.config import load_config
-
-    transient = Path(__file__).parent / "data" / "transient-config.yaml"
-    with load_config(transient) as cfg:
-        dbf = cfg.paths.database
-        if dbf.exists():
-            dbf.unlink()
-        yield cfg
+def config_writable():
+    yield from transient_config("config-writable.yaml")
 
 
 @fixture
-def database(config):
-    return config.database
+def config_readonly():
+    yield from transient_config("config-readonly.yaml")
+
+
+@fixture
+def config_empty():
+    yield from transient_config("config-empty.yaml")
