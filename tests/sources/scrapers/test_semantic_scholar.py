aaf441ae2ea8b4a8feec587393182ef06d713341
Olivier Breuleux <breuleux@gmail.com>
2022-10-26 16:09:15 -0400

    Improvements in tests and test dbs

--- tests/sources/scrapers/test_semantic_scholar.py
+++ tests/sources/scrapers/test_semantic_scholar.py
@@ -1,22 +1,26 @@
-import json
-from pathlib import Path
-
 from pytest import fixture
 
 from paperoni.sources.scrapers.semantic_scholar import SemanticScholarScraper
+from .utils import Artifacts
 
 
 @fixture(scope="module")
 def artifacts():
-    return json.load(
-        open(Path(__file__).parent / "semantic_scholar_artifacts.json")
+    return Artifacts("semantic_scholar_artifacts")
+
+
+@fixture
+def scraper(config_empty):
+    return SemanticScholarScraper(config_empty, config_empty.database)
+
+
+def test_query_title(scraper, artifacts):
+    assert artifacts["autodiff_query"].isin(
+        scraper.query(title=["automatic differentiation in ml where we are"])
     )
 
 
-def test_query(config, database, artifacts):
-    ss = SemanticScholarScraper(config, database)
-    for entry in ss.query(
-        title=["automatic differentiation in ml where we are"]
-    ):
-        assert json.loads(entry.tagged_json()) == artifacts["autodiff_query"]
-        break
+def test_query_author(scraper, artifacts):
+    assert artifacts["autodiff_query"].isin(
+        scraper.query(author=["Olivier Breuleux"])
+    )
--- tests/test_replay.py
+++ tests/test_replay.py
@@ -3,6 +3,6 @@ import coleo
 from paperoni.cli import replay
 
 
-def test_replay(transient_config):
+def test_replay(config_empty):
     with coleo.setvars(before="90"):
         replay()
