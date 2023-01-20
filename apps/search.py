ac28a185a5432937970d1c7cc82121895cc6de2d
Olivier Breuleux <breuleux@gmail.com>
2023-01-20 14:11:38 -0500

    Add simple search app

---
+++ apps/search.py
@@ -0,0 +1,80 @@
+"""Simple search app.
+
+Run with `uvicorn apps.search:app`
+"""
+
+import asyncio
+import os
+from pathlib import Path
+
+from hrepr import H
+from sqlalchemy import select
+from starbear import Queue, bear
+
+from paperoni.config import load_config
+from paperoni.db import schema as sch
+from paperoni.display import html
+
+here = Path(__file__).parent
+
+
+async def regenerator(queue, regen, reset):
+    gen = regen()
+    done = False
+    while True:
+        if done:
+            inp = await queue.get()
+        else:
+            try:
+                inp = await asyncio.wait_for(queue.get(), 0.01)
+            except (asyncio.QueueEmpty, asyncio.exceptions.TimeoutError):
+                inp = None
+
+        if inp is not None:
+            new_gen = regen(inp)
+            if new_gen is not None:
+                done = False
+                gen = new_gen
+                reset()
+                continue
+
+        try:
+            element = next(gen)
+        except StopIteration:
+            done = True
+            continue
+
+        yield element
+
+
+@bear
+async def app(page):
+    q = Queue()
+    page["head"].print(
+        H.link(rel="stylesheet", href=here.parent / "paperoni" / "default.css")
+    )
+    area = H.div["area"]().autoid()
+    page.print(H.input(oninput=q))
+    page.print(area)
+
+    def regen(event=None):
+        title = "neural" if event is None else event.arg["value"]
+        return generate(title)
+
+    def generate(title):
+        stmt = select(sch.Paper)
+        stmt = stmt.filter(sch.Paper.title.like(f"%{title}%"))
+        results = list(db.session.execute(stmt))
+        for (r,) in results:
+            yield r
+
+    with load_config(os.environ["PAPERONI_CONFIG"]) as cfg:
+        with cfg.database as db:
+            regen = regenerator(
+                queue=q,
+                regen=regen,
+                reset=page[area].clear,
+            )
+            async for result in regen:
+                div = html(result)
+                page[area].print(div)
