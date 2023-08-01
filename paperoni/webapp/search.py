556bfd114a793cc0b7f71335928eebdcc674d9f9
Simnol22 <simonkirbi@hotmail.com>
2023-08-01 17:11:25 -0400

    separation of search code, and search_interface code

--- paperoni/webapp/search.py
+++ paperoni/webapp/search.py
@@ -13,12 +13,11 @@ from starbear import ClientWrap, Queue, bear
 from paperoni.config import load_config
 from paperoni.db import schema as sch
 from paperoni.display import html
-
+from .common import search_interface
 here = Path(__file__).parent
 
-
-async def regenerator(queue, regen, reset):
-    gen = regen()
+async def regenerator(queue, regen, reset,db):
+    gen = regen(db=db)
     done = False
     while True:
         if done:
@@ -30,7 +29,7 @@ async def regenerator(queue, regen, reset):
                 inp = None
 
         if inp is not None:
-            new_gen = regen(inp)
+            new_gen = regen(inp,db)
             if new_gen is not None:
                 done = False
                 gen = new_gen
@@ -55,26 +54,15 @@ async def app(page):
         H.link(rel="stylesheet", href=here.parent / "default.css")
     )
     area = H.div["area"]().autoid()
-    page.print(H.input(oninput=debounced))
+    page.print(H.input(name="title", placeholder="Title", oninput=debounced))
     page.print(area)
-
-    def regen(event=None):
-        title = "neural" if event is None else event["value"]
-        return generate(title)
-
-    def generate(title):
-        stmt = select(sch.Paper)
-        stmt = stmt.filter(sch.Paper.title.like(f"%{title}%"))
-        results = list(db.session.execute(stmt))
-        for (r,) in results:
-            yield r
-
     with load_config(os.environ["PAPERONI_CONFIG"]) as cfg:
         with cfg.database as db:
             regen = regenerator(
                 queue=q,
-                regen=regen,
+                regen=search_interface,
                 reset=page[area].clear,
+                db=db,
             )
             async for result in regen:
                 div = html(result)
