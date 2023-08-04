3232e4dedddb656c181391cfece01693492b8e43
Olivier Breuleux <breuleux@gmail.com>
2023-08-04 16:05:37 -0400

    Optional data sorting in isin() test function

--- tests/sources/scrapers/utils.py
+++ tests/sources/scrapers/utils.py
@@ -1,3 +1,6 @@
+from ovld import ovld
+
+
 def controller_from_generator(genfn):
     gen = genfn()
     next(gen)
@@ -8,11 +11,31 @@ def controller_from_generator(genfn):
     return controller
 
 
-def isin(data_regression, results, ignore=[], basename=None, **filter):
+@ovld
+def sort_data(d: dict):
+    return dict(sorted([(k, sort_data(v)) for k, v in d.items()]))
+
+
+@ovld
+def sort_data(li: list):
+    return sorted(li, key=lambda x: str(x))
+
+
+@ovld
+def sort_data(obj: object):
+    return obj
+
+
+def isin(
+    data_regression, results, ignore=[], basename=None, sort=False, **filter
+):
     for r in results:
         if any(getattr(r, k) != v for k, v in filter.items()):
             continue
         r = r.tagged_dict()
+        if sort:
+            # Sort the data to avoid non-deterministic ordering issues
+            r = sort_data(r)
         for k in ignore:
             del r[k]
         data_regression.check(r, basename=basename)
