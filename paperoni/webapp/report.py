14e0c8c9c417efc3d67c81c8ec7aaa5382664028
Olivier Breuleux <breuleux@gmail.com>
2023-09-28 16:37:16 -0400

    Finalize csv report

--- paperoni/webapp/report.py
+++ paperoni/webapp/report.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from starlette.responses import JSONResponse, StreamingResponse
 
 from ..db.model_export import export
+from ..display import expand_links
 from .common import SearchGUI, config
 
 
@@ -51,14 +52,56 @@ class JSONFormatter(PaperFormatter):
 
 class CSVFormatter(PaperFormatter):
     def __init__(self):
-        super().__init__(pre="", join="\n", post="", media_type="text/csv")
+        fields = [
+            "title",
+            "authors",
+            "venue",
+            "date",
+            "status",
+            "link",
+            "pdf",
+            "excerpt",
+        ]
+        super().__init__(
+            pre=",".join(fields) + "\n",
+            join="\n",
+            post="\n",
+            media_type="text/csv",
+        )
+
+    def quote(self, s):
+        s = str(s)
+        if '"' in s:
+            s = s.replace('"', r"\"")
+        if "," in s or "\n" in s or '"' in s:
+            s = f'"{s}"'
+        return s
 
     def destination(self):
         dest = super().destination()
         return f"{dest}.csv"
 
     def process(self, paper):
-        return paper.title
+        rels = [
+            rel
+            for rel in paper.releases
+            if rel.status not in ("submitted", "preprint")
+        ] or paper.releases
+        lnks = expand_links(paper.links)
+        pdfs = [url for ty, url in lnks if ty.endswith("pdf")]
+        row = {
+            "title": paper.title,
+            "authors": ",".join(a.author.name for a in paper.authors),
+            "venue": rels[0].venue.name,
+            "date": datetime.fromtimestamp(rels[0].venue.date).strftime(
+                "%Y-%m-%d"
+            ),
+            "status": rels[0].status,
+            "link": lnks[0][1],
+            "pdf": pdfs[0] if pdfs else "",
+            "excerpt": "".join(getattr(paper, "excerpt", ("", "", ""))),
+        }
+        return ",".join(map(self.quote, row.values()))
 
 
 formatters = {
