1e51733ef6f47240b596d6cedca46cc85234bc09
Olivier Breuleux <breuleux@gmail.com>
2024-02-02 15:05:49 -0500

    Fix permissions

--- paperoni/webapp/admin/permissions.py
+++ paperoni/webapp/admin/permissions.py
@@ -4,7 +4,8 @@ from ..common import FileEditor, mila_template
 @mila_template(help="/help#permissions")
 async def app(page, box):
     """Update permissions."""
-    await FileEditor(page.app.grizzlaxy.permissions).run(box)
+    # yikes.
+    await FileEditor(page.instance.mother.app.grizzlaxy.permissions).run(box)
 
 
 ROUTES = app
