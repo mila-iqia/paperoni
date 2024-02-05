6604344aa6c7392f6ad210fd86b06744be26224a
Olivier Breuleux <breuleux@gmail.com>
2024-02-05 14:38:04 -0500

    Fix tests using new configuration

--- tests/test_replay.py
+++ tests/test_replay.py
@@ -1,8 +1,17 @@
-import coleo
-
-from paperoni.cli import replay
-
-
 def test_replay(config_empty):
-    with coleo.setvars(before="90"):
-        replay()
+    def check():
+        with config_empty.database as db:
+            results = list(
+                db.session.execute(
+                    'SELECT title FROM paper WHERE title LIKE "%Machine Learning%"'
+                )
+            )
+            return results == [
+                (
+                    "Machine Learning for Combinatorial Optimization: a Methodological Tour d'Horizon",
+                )
+            ]
+
+    assert not check()
+    config_empty.database.replay(before="90")
+    assert check()
