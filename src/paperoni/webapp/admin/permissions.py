315771c13fc37198bc4d6dbcf2e900d0ec47ce8a
Olivier Breuleux <breuleux@gmail.com>
2024-09-18 15:42:23 -0400

    Finish adapting to new starbear

--- src/paperoni/webapp/admin/permissions.py
+++ src/paperoni/webapp/admin/permissions.py
@@ -5,4 +5,4 @@ from ..common import FileEditor, mila_template
 async def __app__(page, box):
     """Update permissions."""
     # yikes.
-    await FileEditor(page.instance.mother.app.grizzlaxy.permissions).run(box)
+    await FileEditor(page.server_instance.plugins["permissions"].permissions).run(box)
