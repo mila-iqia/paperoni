0b506283445ca6d0ff9cc63b379293ab02d59c25
Olivier Breuleux <breuleux@gmail.com>
2024-12-11 15:40:31 -0500

    Remove logs viewer (we use Loki instead)

--- src/paperoni/webapp/admin/logs.py
+++
@@ -1,9 +0,0 @@
-from ...config import papconf
-from ..common import LogsViewer, mila_template
-
-
-@mila_template(help="/help#logs")
-async def __app__(page, box):
-    """Monitor systemd logs."""
-    services = papconf.services or {}
-    await LogsViewer(services).run(box)
