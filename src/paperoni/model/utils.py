3e137b5db7db37ff6b5308aad3ff56b63fab09e2
Olivier Breuleux <breuleux@gmail.com>
2025-11-10 17:06:32 -0500

    Change paper_has_updated

--- src/paperoni/model/utils.py
+++ src/paperoni/model/utils.py
@@ -1,13 +1,18 @@
-from paperoni.model.classes import Paper
+from .classes import Paper
 
 
 def paper_has_updated(paper: Paper, new_paper: Paper) -> bool:
-    return (
-        paper.title != new_paper.title
-        # We do not check the authors name as a minor name change would flag the
-        # paper as updated
-        or len({author.display_name for author in paper.authors})
-        != len({author.display_name for author in new_paper.authors})
-        # There are new links in the new paper
-        or bool({link for link in new_paper.links} - {link for link in paper.links})
-    )
+    # We only check the links list because it is purely incremental; checking the
+    # title or the authors will cause the paper to be updated every time if several
+    # sources have slight disagreements
+    return bool({link for link in new_paper.links} - {link for link in paper.links})
+
+    # return (
+    #     normalize_title(paper.title) != normalize_title(new_paper.title)
+    #     # We do not check the authors name as a minor name change would flag the
+    #     # paper as updated
+    #     or len({author.display_name for author in paper.authors})
+    #     != len({author.display_name for author in new_paper.authors})
+    #     # There are new links in the new paper
+    #     or bool({link for link in new_paper.links} - {link for link in paper.links})
+    # )
