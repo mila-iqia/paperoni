0f25563adcac6d324d62d0d0b21343cec5373ce3
Olivier Breuleux <breuleux@gmail.com>
2026-01-22 16:23:17 -0500

    Add arxiv search by title

--- src/paperoni/refinement/title.py
+++ src/paperoni/refinement/title.py
@@ -6,7 +6,7 @@ from ..config import config
 from ..discovery.openalex import OpenAlexQueryManager
 from ..get import ERRORS
 from .fetch import register_fetch
-from .formats import paper_from_crossref
+from .formats import paper_from_crossref, papers_from_arxiv
 
 
 @register_fetch
@@ -53,9 +53,36 @@ async def openalex_title(typ: Literal["title"], link: str):
 
     async for paper in qm.works(
         filter=f"display_name.search:{title.strip().replace(',', '')}",
-        data_version="1",
+        data_version="2",
         limit=1,
     ):
         paper = paper.paper
+        if paper.title == title:
+            return paper
+
+
+@register_fetch
+async def arxiv_title(type: Literal["title"], link: str):
+    """Fetch from ArXiv by title search."""
+
+    title = link
+
+    try:
+        soup = await config.fetch.read(
+            "https://export.arxiv.org/api/query",
+            params={
+                "search_query": f'title:"{title}"',
+                "start": "0",
+                "max_results": "5",
+            },
+            format="xml",
+        )
+    except ERRORS as exc:  # pragma: no cover
+        if exc.response.status_code == 404:
+            return None
+        else:
+            raise
+
+    for paper in papers_from_arxiv(soup):
         if paper.title == title:
             return paper
