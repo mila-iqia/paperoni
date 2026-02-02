27d18987d1b7be50b2e6679d63c53b7e86fd9a44
Olivier Breuleux <breuleux@gmail.com>
2026-02-02 17:12:12 -0500

    Add peer_reviewed and sort_releases operations

--- src/paperoni/operations.py
+++ src/paperoni/operations.py
@@ -58,3 +58,46 @@ def operation(fn):
         )
 
     return deco
+
+
+#####################
+# Operation helpers #
+#####################
+
+
+def release_status_order(release: Release):
+    name = release.venue.name.lower()
+    if release.status.lower() in ("submitted", "withdrawn", "rejected"):
+        return -2
+    elif (
+        release.status == "preprint"
+        or not name.strip()
+        or name == "n/a"
+        or "rxiv" in name
+    ):
+        return -1
+    elif "workshop" in name:
+        return 0
+    else:
+        return 1
+
+
+def is_peer_reviewed_release(release: Release):
+    return release_status_order(release) > 0
+
+
+######################
+# Defined operations #
+######################
+
+
+@flag_setter("peer-reviewed")
+def peer_reviewed(p: Paper):
+    return any(is_peer_reviewed_release(r) for r in p.releases)
+
+
+@operation
+def sort_releases(p: Paper):
+    releases = [(release, release_status_order(release)) for release in p.releases]
+    releases.sort(key=lambda entry: -entry[1])
+    return replace(p, releases=[r for r, _ in releases])
