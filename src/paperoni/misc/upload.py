738f19130568f26c3a7503f9d75d7f39d250d652
Olivier Breuleux <breuleux@gmail.com>
2026-03-13 16:32:43 -0400

    Update upload.py

--- src/paperoni/misc/upload.py
+++ src/paperoni/misc/upload.py
@@ -1,17 +1,30 @@
 import asyncio
+import hashlib
 import json
 import time
 from dataclasses import dataclass
+from datetime import date, datetime, timezone
 
 import gifnoc
 import requests
 from outsight.ops import buffer, enumerate
+from ovld import Medley, call_next
 from requests.auth import HTTPBasicAuth
-from serieux import serialize
+from serieux import Context, Serieux
 from serieux.features.encrypt import Secret
 
+from paperoni.utils import expand_links_dict
+
 from ..config import config
-from ..model.classes import Paper
+from ..model.classes import (
+    Author,
+    DatePrecision,
+    Paper,
+    PaperAuthor,
+    Release,
+    Venue,
+    dataclass,
+)
 
 
 @dataclass
@@ -34,6 +47,8 @@ class UploadOptions:
     block_size: int = 100
     # Number of seconds to wait between two uploads
     block_pause: float = 10
+    # Start date
+    start_date: date = None
 
     def auth(self):
         return self.user and HTTPBasicAuth(username=self.user, password=self.password)
@@ -45,33 +60,90 @@ upload_options = gifnoc.define(
 )
 
 
+class UploadSerieux(Medley):
+    def serialize(self, t: type[Paper], obj: Paper, ctx: Context):
+        ser = call_next(t, obj, ctx)
+        valid = True
+        if not obj.releases or obj.releases[0].peer_review_status not in {
+            "peer-reviewed",
+            "preprint",
+            "workshop",
+        }:
+            valid = False
+        ser["releases"] = ser["releases"][:1]
+        ser["validated"] = valid
+        ser["flags"] = [{"name": "validation", "value": 1}]
+        ser["citation_count"] = 0
+        ser["excerpt"] = None
+        ser["paper_id"] = ser["id"]
+        del ser["id"]
+        del ser["key"]
+        del ser["info"]
+        del ser["score"]
+        del ser["version"]
+        ser["links"] = [lnk for lnk in expand_links_dict(obj.links) if "url" in lnk]
+        return ser
+
+    def serialize(self, t: type[Venue], obj: Venue, ctx: Context):
+        ser = call_next(t, obj, ctx)
+        iso_date = ser["date"]
+        timestamp = int(
+            datetime.fromisoformat(iso_date).replace(tzinfo=timezone.utc).timestamp()
+        )
+        ser["date"] = {
+            "text": DatePrecision.format(date=obj.date, precision=obj.date_precision),
+            "timestamp": timestamp,
+            "precision": ser["date_precision"],
+        }
+        ser["venue_id"] = hashlib.md5(obj.name.encode()).hexdigest()
+        del ser["date_precision"]
+        del ser["short_name"]
+        del ser["aliases"]
+        del ser["open"]
+        del ser["peer_reviewed"]
+        return ser
+
+    def serialize(self, t: type[Release], obj: Release, ctx: Context):
+        ser = call_next(t, obj, ctx)
+        ser["peer_reviewed"] = ser["peer_review_status"] == "peer-reviewed"
+        del ser["peer_review_status"]
+        return ser
+
+    def serialize(self, t: type[PaperAuthor], obj: PaperAuthor, ctx: Context):
+        ser = call_next(t, obj, ctx)
+        ser["author"]["name"] = ser["display_name"]
+        del ser["display_name"]
+        ser["affiliations"] = []
+        return ser
+
+    def serialize(self, t: type[Author], obj: Author, ctx: Context):
+        ser = call_next(t, obj, ctx)
+        ser["author_id"] = hashlib.md5(obj.name.encode()).hexdigest()
+        del ser["aliases"]
+        return ser
+
+
+srx = (Serieux + UploadSerieux)()
+
+
 async def main():
     if not upload_options.url and not upload_options.only_dump:
         exit("No URL to upload to.")
 
     async for i, block in enumerate(
-        buffer(config.collection.search(), count=upload_options.block_size)
+        buffer(
+            config.collection.search(start_date=upload_options.start_date),
+            count=upload_options.block_size,
+        )
     ):
-
-        def _export(paper: Paper):
-            ser = serialize(Paper, paper)
-            valid = "valid" in paper.flags
-            if not paper.releases or paper.releases[0].peer_review_status not in {
-                "peer-reviewed",
-                "preprint",
-                "workshop",
-            }:
-                valid = False
-            ser["validated"] = valid
-            return ser
-
         if i > 0 and upload_options.block_pause:
             time.sleep(upload_options.block_pause)
 
-        exports = [_export(paper) for paper in block]
+        exports = [srx.serialize(Paper, paper) for paper in block]
         if upload_options.only_dump:
             serialized = json.dumps(exports, indent=4)
             print(serialized)
+            break
 
         else:
             print(len(exports), "papers will be uploaded.")
